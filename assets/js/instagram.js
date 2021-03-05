718eb1766b143984b1fd08ef9879117e54640c5c
jtmar28 <justinmartinez0123@gmail.com>
2021-03-05 14:44:44 -0600

    Added target blank
    Need to add in a template so instagram is opened in new tab

--- assets/js/instagram.js
+++ assets/js/instagram.js
@@ -7,6 +7,7 @@ fetch('https://ig.instant-tokens.com/users/a894c61e-1c13-4978-94cd-f9fe73fbb5d2/
           limit: 6,
           resolution: 'low_resolution',
           accessToken: data.Token,
+          template: '<a href="{{link}}" target="_blank"><img title="{{caption}}" src="{{image}}" /><div></div></a>',
         });
         feed.run();
 });
