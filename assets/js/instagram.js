b486984bfa1502a4c7d71d2bb1d2c59e773f3cc7
jtmar28 <justinmartinez0123@gmail.com>
2021-04-21 12:44:31 -0500

    update instagram token for next term
    This is updating the instagram token for fall 2021

--- assets/js/instagram.js
+++ assets/js/instagram.js
@@ -1,4 +1,4 @@
-fetch('https://ig.instant-tokens.com/users/a894c61e-1c13-4978-94cd-f9fe73fbb5d2/instagram/17841404340334440/token?userSecret=ioiujswnfkqjar206l8xs')
+fetch('https://ig.instant-tokens.com/users/a894c61e-1c13-4978-94cd-f9fe73fbb5d2/instagram/17841404340334440/token?userSecret=52x5h0jrs231k4gr6f08ds')
   .then(resp => resp.json())
   .then(data => {
         var feed = new Instafeed({
